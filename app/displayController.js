2944290fd5a190cb522969621adeae518ba49a26
aegis945 <artiom.lekincev@gmail.com>
2023-12-19 15:23:34 +0200

    add styles

--- app/displayController.js
+++ app/displayController.js
@@ -1,11 +1,17 @@
 const DisplayController = (() => {
+    const messageElement = document.querySelector("#message");
+
     const renderMessage = (message) => {
-        document.querySelector("#message").innerHTML = message; 
-    }
+        messageElement.innerHTML = message;
+        messageElement.classList.add("active");
+        setTimeout(() => {
+            messageElement.classList.remove("active");
+        }, 5000);
+    };
+
     return {
         renderMessage
     }
 })();
-
 
 export default DisplayController;
